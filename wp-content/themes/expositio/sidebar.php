0785cfebad0b9d2b91b525092cc68f0961202a21
santiago reyes <santiago@intervalo.co>
2013-02-27 20:35:59 -0500

    Revert "cagada drop down"
    This reverts commit 23556d63a5f72d0c54b945d758cfde188575fe52.

--- wp-content/themes/expositio/sidebar.php
+++ wp-content/themes/expositio/sidebar.php
@@ -33,18 +33,6 @@
 								'post_type'              	=> '',
 								'orderby'                 => 'count',
 								'order'                   => 'DESC'
-								<li id="categories">
-	<h2><?php _e('Posts by Category'); ?></h2>
-	<form action="<?php bloginfo('url'); ?>/" method="get">
-	<div>
-<?php
-$select = wp_dropdown_categories('show_option_none=Select category&show_count=1&orderby=name&echo=0');
-$select = preg_replace("#<select([^>]*)>#", "<select$1 onchange='return this.form.submit()'>", $select);
-echo $select;
-?>
-	<noscript><div><input type="submit" value="View" /></div></noscript>
-	</div></form>
-</li>
 							);
 							
 							$the_query = new WP_Query( $arrArgs );
