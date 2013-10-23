6d5eb0e6487668aa45ab619557ae9308e7836b9b
Anabelle Handdoek <ana@gathacol.net>
2013-10-23 17:08:13 -0500

    change js

--- wp-content/themes/expositio/js/main.js
+++ wp-content/themes/expositio/js/main.js
@@ -1,10 +1,11 @@
 jQuery(document).ready(function($){
 	$('.wpscls-cate-post h4').hover( 
 		function(){
+			$('.wpscls-cate-post ul').hide();
 			$(this).next('ul').show();
 		}, 
 		function(){
-			$(this).next('ul').hide();
+			// $(this).next('ul').hide();
 		} 
 	);
 });
