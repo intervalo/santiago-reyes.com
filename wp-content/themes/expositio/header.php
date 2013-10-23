bef9b99503deb9253c89d8315861178ca6d5d44f
Anabelle Handdoek <ana@gathacol.net>
2013-10-23 16:19:13 -0500

    enable modernizr

--- wp-content/themes/expositio/header.php
+++ wp-content/themes/expositio/header.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN" "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml" dir="<?php bloginfo('text_direction'); ?>" xml:lang="<?php bloginfo('language'); ?>">
+<html xmlns="http://www.w3.org/1999/xhtml" dir="<?php bloginfo('text_direction'); ?>" xml:lang="<?php bloginfo('language'); ?>" class="no-js">
 	<head>
 		<meta http-equiv="Content-Type" content="<?php bloginfo('html_type'); ?>; charset=<?php bloginfo('charset'); ?>" />
 		<title>
@@ -78,7 +78,7 @@
 		
 		</style>
 	</head>
-	<body <?php body_class('no-js'); ?>>
+	<body <?php body_class(); ?>>
 		<!--[if gt IE 8]> <div id="wps-site-wrapper" class="wpscls-ie wpscls-ie9"> <![endif]-->
 		<!--[if IE 8]> <div id="wps-site-wrapper" class="wpscls-ie wpscls-ie8"> <![endif]-->
 		<!--[if IE 7]> <div id="wps-site-wrapper" class="wpscls-ie wpscls-ie7"> <![endif]-->
