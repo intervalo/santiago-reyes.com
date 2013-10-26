b7441b0274cdaaaa747b462edf2e687990686a6b
Anabelle Handdoek <ana@gathacol.net>
2013-10-26 09:28:24 -0500

    keep current cat open

--- wp-content/themes/expositio/js/main.js
+++ wp-content/themes/expositio/js/main.js
@@ -1,8 +1,12 @@
 jQuery(document).ready(function($){
+	
+	$('.wpscls-cate-post ul').has('.current').show();
+
 	$('.wpscls-cate-post h4').on('click', 
 		function(){
 			$('.wpscls-cate-post ul').hide();
 			$(this).next('ul').show();
 		} 
 	);
+	
 });
