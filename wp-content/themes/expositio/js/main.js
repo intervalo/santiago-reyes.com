6109b1e2b8eb924a4ec8bacbd949080f5ca48ee4
Anabelle Handdoek <ana@gathacol.net>
2013-10-26 09:25:49 -0500

    submenus on click

--- wp-content/themes/expositio/js/main.js
+++ wp-content/themes/expositio/js/main.js
@@ -1,11 +1,8 @@
 jQuery(document).ready(function($){
-	$('.wpscls-cate-post h4').hover( 
+	$('.wpscls-cate-post h4').on('click', 
 		function(){
 			$('.wpscls-cate-post ul').hide();
 			$(this).next('ul').show();
-		}, 
-		function(){
-			// $(this).next('ul').hide();
 		} 
 	);
 });
