646da14ea06f0d15c60509d214fa869290ebfd4d
Anabelle Handdoek <ana@gathacol.net>
2013-10-26 09:44:57 -0500

    text archive

---
+++ wp-content/themes/expositio/archive-text.php
@@ -0,0 +1,11 @@
+<?php 
+/* 
+Archive Template: Text archive
+*/ 
+?>
+
+<?php get_header(); ?>
+			
+			<?php locate_template( array( 'tmpl_text.php'), true, false ) ?>
+				
+<?php get_footer(); ?>
